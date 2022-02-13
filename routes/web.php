684086dc279de3491bdcccf328df1b5fe63c992f
Nanderson Castro <nandokstro@gmail.com>
2022-02-13 11:55:52 -0300

    Iniciando projeto

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
+Route::controller(\App\Http\Controllers\RestaurantController::class)
+        ->name('restaurants.')
+        ->prefix('restaurants')
+        ->group(function(){
+
+            Route::get('/', 'index')->name('index');
+            Route::get('/create', 'create')->name('create');
+
+        });
+
