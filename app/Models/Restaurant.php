6ab1945d78a0ac6b57466eb0b5c4610e9aa9dfa6
Nanderson Castro <nandokstro@gmail.com>
2022-02-18 21:37:42 -0300

    Adicionando novidade melhorias mutators e accessors

---
+++ app/Models/Restaurant.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+
+class Restaurant extends Model
+{
+    use HasFactory;
+
+    
+    public function name(): Attribute
+    {
+        return new Attribute(
+
+            get: fn($value) => strtoupper($value),
+
+            set: fn($value) => $value . ' ok '
+        );
+    }
+}
